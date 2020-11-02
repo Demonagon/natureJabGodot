e71a91d542425f18f21df3c73ac3eff48a97b2e0
pacome <pacome.perrotin@etu.univ-amu.fr>
2020-11-02 15:48:28 +0100

    Added treepartskin

---
+++ tree/skin/TreePartSkin.gd
@@ -0,0 +1,9 @@
+extends Spatial
+
+class_name TreePartSkin
+
+func setDistance(distance : float):
+  print("WARNING : setDistance not implemented for TreePartSkin instance")
+  
+func setSize(size : float):
+  print("WARNING : setSize not implemented for TreePartSkin instance")
